efd01b01a64c5101edf9ea67aac7d92ed410b15d
yuan.xu <yuan.xu@majorbio.com>
2023-09-03 23:51:42 +0800

    GO语言学习

--- go/demo.go
+++ go/demo.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Println("hello go")
+	arr1 := [5]string{"red", "blue", "green", "yello", "pink"}
+	arr2 := arr1
+	arr1[4] = "black"
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+	var arr3 [5]*string
+	arr4 := [5]*string{new(string), new(string), new(string), new(string), new(string)}
+	*arr4[0] = "red"
+	arr3 = arr4
+	fmt.Println(arr3)
+	fmt.Println(arr4)
+	slice := []int{10, 20, 30, 40, 50}
+	newslice := slice[1:3]
+	slice[6] = 45
+	fmt.Println(newslice)
 }
